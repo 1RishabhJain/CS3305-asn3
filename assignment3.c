b9c22c7b49f95b3e0c3e2104f7650922718e7eae
Rishabh Jain <rjain57@uwo.ca>
2021-10-27 12:42:49 -0400

    Update assignment3.c

--- assignment3.c
+++ assignment3.c
@@ -12,18 +12,22 @@
 
 // Creating array to be used by sprintf in main and functions
 char output[256];
+
 // Pipe
 int port[2];
+
 // Integer holding number of characters to be read/written from/to pipe
 int pipeSize;
 
-char array[0];
 // Integer S is sum of the two args
 int S;
 
 // This function is executed by thread 100
 void *sum(void *thread_id) {
     int *id = (int*)thread_id;
+
+    // Array for use while reading from pipe
+    char array[pipeSize];
 
     // Thread reads from pipe
     read(port[0], array, pipeSize);
@@ -41,13 +45,14 @@ void *sum(void *thread_id) {
 
     // Adds X and Y
     S = X + Y;
-    sprintf(output, "%d", S);
+    sprintf(array, "%d", S);
 
-    pipeSize = strlen(output);
+    pipeSize = strlen(array);
     // Write to pipe and print statement
-    write(port[1], output, pipeSize);
-    sprintf(output, "thread (TID %d) writes X + Y = %d to the pipe", *id, S);
+    write(port[1], array, pipeSize);
+    sprintf(output, "thread (TID %d) writes X+Y = %d to the pipe", *id, S);
     puts(output);
+
     return 0;
 }
 
@@ -55,30 +60,33 @@ void *sum(void *thread_id) {
 void *odd_even(void *thread_id) {
     int *id = (int*)thread_id;
 
+    // Array for use while reading from pipe
+    char array[pipeSize];
+
     // Thread reads from pipe
-    read(port[0], output, pipeSize);
+    read(port[0], array, pipeSize);
+
     // Convert S to int
-    S = atoi(output);
+    S = atoi(array);
 
     // Print the statement and the values
-    sprintf(output, "thread (TID %d) reads X + Y = %d from the pipe", *id, S);
+    sprintf(output, "thread (TID %d) reads X+Y = %d from the pipe", *id, S);
     puts(output);
 
     // Check if S is even or odd
     if (S % 2 == 0) {
-        sprintf(output, "thread (TID %d) identifies X + Y = %d as an even number", *id, S);
+        sprintf(output, "thread (TID %d) identifies X+Y = %d as an even number", *id, S);
         puts(output);
     }
     else {
-        sprintf(output, "thread (TID %d) identifies X + Y = %d as an odd number", *id, S);
+        sprintf(output, "thread (TID %d) identifies X+Y = %d as an odd number", *id, S);
         puts(output);
     }
 
-    sprintf(output, "%d", S);
-    pipeSize = strlen(output);
+    sprintf(array, "%d", S);
     // Write to pipe and print statement
-    write(port[1], output, pipeSize);
-    sprintf(output, "thread (TID %d) writes X + Y = %d to the pipe", *id, S);
+    write(port[1], array, pipeSize);
+    sprintf(output, "thread (TID %d) writes X+Y = %d to the pipe", *id, S);
     puts(output);
 
     return 0;
@@ -88,25 +96,21 @@ void *odd_even(void *thread_id) {
 void *digit_count(void *thread_id) {
     int *id = (int*)thread_id;
 
+    // Array for use while reading from pipe
+    char array[pipeSize];
+
     // Thread reads from pipe
-    read(port[0], output, pipeSize);
+    read(port[0], array, pipeSize);
+
     // Convert S to int
-    S = atoi(output);
-    int sCopy = S;
+    S = atoi(array);
 
     // Print the statement and the values
-    sprintf(output, "thread (TID %d) reads X + Y = %d from the pipe", *id, S);
+    sprintf(output, "thread (TID %d) reads X+Y = %d from the pipe", *id, S);
     puts(output);
 
-    // Count the number of digits in S
-    int count = 0;
-    while (sCopy != 0) {
-        sCopy = sCopy / 10;
-        ++count;
-    }
-
-    // Print the statement and the values
-    sprintf(output, "thread (TID %d) identifies X + Y = %d as a %d digit number", *id, S, count);
+    // Print the statement and the values (the number of digits in S is pipeSize)
+    sprintf(output, "thread (TID %d) identifies X+Y = %d as a %d digit number", *id, S, pipeSize);
     puts(output);
 
     return 0;
@@ -128,14 +132,17 @@ int main(int argc, char *argv[2]) {
     char *yArg = argv[2];
     int Y = atoi(yArg);
 
+    // Array for use while reading from pipe
+    char array[strlen(argv[1]) + strlen(argv[1]) + 2];
+
     // Print the received values
     sprintf(output, "parent (PID %d) receives X = %d and Y = %d from the user", parent_pid, X, Y);
     puts(output);
 
-    // Parent writes X to the pipe
-    sprintf(output, "%d,%d", X, Y);
-    pipeSize = strlen(output);
-    write(port[1], output, strlen(output));
+    // Parent writes X and Y to the pipe
+    sprintf(array, "%d,%d", X, Y);
+    pipeSize = strlen(array);
+    write(port[1], array, pipeSize);
     sprintf(output, "parent (PID %d) writes X = %d and Y = %d to the pipe", parent_pid, X, Y);
     puts(output);
 
@@ -146,6 +153,7 @@ int main(int argc, char *argv[2]) {
     // Thread 1 (ID 100)
     if (pthread_create(&thread_1, NULL, sum, &thread_id_1)) {
         sprintf(output, "Error while creating thread 1\n");
+        puts(output);
         exit(1);
     }
     pthread_join(thread_1, NULL);
@@ -153,6 +161,7 @@ int main(int argc, char *argv[2]) {
     // Thread 2 (ID 101)
     if (pthread_create(&thread_2, NULL, odd_even, &thread_id_2)) {
         sprintf(output, "Error while creating thread 2\n");
+        puts(output);
         exit(1);
     }
     pthread_join(thread_2, NULL);
@@ -160,6 +169,7 @@ int main(int argc, char *argv[2]) {
     // Thread 3 (ID 102)
     if (pthread_create(&thread_3, NULL, digit_count, &thread_id_3)) {
         sprintf(output, "Error while creating thread 3\n");
+        puts(output);
         exit(1);
     }
     pthread_join(thread_3, NULL);
